b31a6369621e1f9cce55f1d1f4bdeb5e96c6f3e1
Dimicek <d.kanjuka@seznam.cz>
2024-05-06 18:42:21 +0200

    Crypto app

---
+++ src/components/Input.vue
@@ -0,0 +1,24 @@
+<template>
+   <input type="number">
+</template>
+
+<script>
+   export default {
+    
+   }
+</script>
+
+<style scoped>
+    input {
+        border-radius: 3px;
+        border: 0;
+        width: 500px;
+        position: relative;
+        top: -50px;
+        font-size: 1.2em;
+        outline: none;
+        padding: 10px 15px;
+        background: #fafafa;
+        color: #333;
+    }
+</style>
